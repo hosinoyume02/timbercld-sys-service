240c06e6818c4de0ba6ef3fa641d749cb1145871
djx <daijx@mucaimatou.com>
2023-05-10 17:27:30 +0800

    'fixmetahanderld'

--- timbercld-core-ws/src/main/java/com/timbercld/ws/system/handler/FieldMetaObjectHandler.java
+++ timbercld-core-ws/src/main/java/com/timbercld/ws/system/handler/FieldMetaObjectHandler.java
@@ -11,7 +11,7 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
-* Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -20,17 +20,27 @@
 
 package com.timbercld.ws.system.handler;
 
+import com.baomidou.mybatisplus.core.metadata.TableFieldInfo;
+import com.baomidou.mybatisplus.core.metadata.TableInfoHelper;
+import com.baomidou.mybatisplus.core.toolkit.Constants;
+import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import com.baomidou.mybatisplus.core.metadata.TableInfo;
+import com.baomidou.mybatisplus.core.toolkit.ObjectUtils;
 import com.timbercld.ws.authority.utils.AuthorityUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.ibatis.reflection.MetaObject;
+import org.apache.ibatis.reflection.SystemMetaObject;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
+import java.util.Objects;
+import java.util.Optional;
 
 /**
  * 公共字段，自动填充值
  *
- * 
+ *
  */
 @Component
 public class FieldMetaObjectHandler implements MetaObjectHandler {
@@ -44,21 +54,20 @@ public class FieldMetaObjectHandler implements MetaObjectHandler {
 
     @Override
     public void insertFill(MetaObject metaObject) {
+        strictInsertFill(metaObject,USER_ID, Long.class, AuthorityUtils.getUserId());
         //创建者
-        this.setFieldValByName(CREATOR_ID, AuthorityUtils.getUserId(), metaObject);
+        setFieldValByName(CREATOR_ID, AuthorityUtils.getUserId(), metaObject);
         //创建时间
         setFieldValByName(CREATE_DATE, new Date(), metaObject);
         //更新者
         setFieldValByName(UPDATER_ID, AuthorityUtils.getUserId(), metaObject);
         //更新时间
         setFieldValByName(UPDATE_DATE, new Date(), metaObject);
-
-        //用户ID
-        setFieldValByName(USER_ID, AuthorityUtils.getUserId(), metaObject);
         //创建者所属部门
-        setFieldValByName(DEPT_ID, AuthorityUtils.getDeptId(), metaObject);
+        strictInsertFill(metaObject , DEPT_ID, Long.class, AuthorityUtils.getDeptId());
         //子系统编码
-        setFieldValByName(SUBSYSTEM_ID, AuthorityUtils.getSubSystemId(), metaObject);
+        strictInsertFill(metaObject, SUBSYSTEM_ID,Long.class , AuthorityUtils.getSubSystemId());
+
 
     }
 
@@ -69,4 +78,6 @@ public class FieldMetaObjectHandler implements MetaObjectHandler {
         //更新时间
         setFieldValByName(UPDATE_DATE, new Date(), metaObject);
     }
+
+
 }
